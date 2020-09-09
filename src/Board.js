22976f6a6e11187570148b0cd7081104b47ee32d
Liron <liron2009@gmail.com>
2020-09-09 08:42:26 +0300

    tiny fixes

--- src/Board.js
+++ src/Board.js
@@ -12,9 +12,13 @@ class Board extends React.Component {
         this.boardSize = boardSize;
         this.subs = new Array(5);
         this.countClicks = 0;
-        this.x1 = null;
-        this.y1 = null;
         
+        this.subsConfig = [
+            { name: 'Submarine', size: 4, count: 1, placed: 0 },
+            { name: 'Cruiser', size: 3, count: 2, placed: 0 },
+            { name: 'Destroyer', size: 2, count: 2, placed: 0 }
+        ];
+
         this.state = {
             board: Array(100).fill(null),
             subsConfig : [
@@ -32,10 +36,6 @@ class Board extends React.Component {
     componentWillReceiveProps(nextProps) {
         this.setState({ board: Array(nextProps.boardSize).fill(null)});
     }
-
-
-    //Sets sub coordinates which recevied by the player
-  
 
     attack = (coords) => {
         this.subs.forEach(sub => {
@@ -57,11 +57,11 @@ class Board extends React.Component {
         if(this.state.board[i] !== null) return;
 
         this.countClicks ++;
-        if(this.countClicks == 1) {
+        if(this.countClicks === 1) {
             this.x1 = Math.floor(i/10);
             this.y1 = i%10;
 
-        } else if (this.countClicks == 2) {
+        } else if (this.countClicks === 2) {
             const subsConfigHolder = [...this.state.subsConfig];
             const board = [...this.state.board];
             this.x2 = Math.floor(i/10);
@@ -80,7 +80,6 @@ class Board extends React.Component {
                                 console.log('set the ship!');
                                 theArray[index].placed ++;
                                 
-
                                 // ship selected from left to right on the same row click
                                 let dozens = this.x1 * 10;
                                 if(this.y1 < this.y2 ) {
@@ -108,7 +107,8 @@ class Board extends React.Component {
                                 return true; 
                             }
                             // console.log(this.subsConfig);
-                        }      
+                        }   
+                        return null;   
                     });
                 }
             } else if (this.y1 === this.y2) {
@@ -124,7 +124,7 @@ class Board extends React.Component {
                                 for(let i=this.x1; i<=this.x2; i++) {
                                     let combinedPoint = i*10 + remainder;
                                     board[combinedPoint] = 'X';
-                                    coords.push(combinedPoint)
+                                    coords.push(combinedPoint);
                                 }
                             this.subs.push(new Sub(singleSub.size, coords));
                             } else {
@@ -144,41 +144,15 @@ class Board extends React.Component {
                             this.countClicks = 0;
                             return true;
                         }
-                    }      
+                    } return false;      
                 });
               }
             this.countClicks = 0;
             }
     }
 
-    // placeShipOnClick(i) {
-        // this.countClicks ++;
-        // if(this.countClicks == 1) {
-        //     this.x1 = i/10;
-        //     this.y1 = i%10;
-        // } else if (this.countClicks == 2) {
-        //     if(!this.singleConfigHolder) {
-        //         this.singleConfigHolder = this.props.getConfigSubs.pop();
-        //     }
-        //     if (this.singleConfigHolder.count) {
-        //         if((this.x1 === i/10) && (this.y1 + i%10 === this.singleConfigHolder.size - 1)) {
-        //             //set ship between 
-
-        //         } 
-        //     }
-        //     }
-        //     this.countClicks = 0;
-        // }
-    //     const tempBoard = [...this.state.board];
-    //     if(!tempBoard[i]) {
-    //         tempBoard[i] = 'X';
-    //         this.setState({board:tempBoard});
-    //     }
-       
-    // }
 
     renderSquare(i) {
-        
       return <Square
                 value={this.state.board[i]} 
                 onClick={()=>{this.twoClicksForPlaceSub(i)}} 
@@ -193,27 +167,35 @@ class Board extends React.Component {
         console.log(this.state.board);
         console.log(this.state.subsConfig); 
 
-        const printSquaresHelper = [...Array(this.props.boardSize).keys()].map(index => {
+        const printBoard = [...Array(this.props.boardSize).keys()].map(index => {
            return this.renderSquare(index);
         });
 
+        // const subsToPlaceList = this.subsConfig.map(sub => {
+        //                      if(sub.count !== sub.placed) {
+        //                       return <li key={sub.name}><span>{sub.count - sub.placed} {sub.name}s  of size {sub.size}</span></li>;
+        //                      }                       
+        //                      return null;      
+        // })
         const subsToPlaceList = this.state.subsConfig && 
                                 this.props.status ==='pre-game' && 
                                 this.state.subsConfig.map(sub => {
-                                if(sub.count !== sub.placed)
-                                return <li key={sub.name}><span>{sub.count - sub.placed} {sub.name}s  of size {sub.size}</span></li>;
-        });
+                                if(sub.count !== sub.placed) {
+                                    return <li key={sub.name}><span>{sub.count - sub.placed} {sub.name}s  of size {sub.size}</span></li>;
+                                }
+                                return null;
+                            });
 
       return (
           <React.Fragment>
             { this.state.subsConfig &&
             <div className="subs-to-place">
-                <h5>You have to add:</h5>
+                <span>Please place on the board:</span>
                 <ul>{subsToPlaceList}</ul>
             </div>
             }       
             <div className="board-container">
-                {printSquaresHelper}
+                {printBoard}
             </div>
 
           </React.Fragment>
--- src/Game.js
+++ src/Game.js
@@ -7,20 +7,21 @@ class Game extends React.Component {
     constructor(players, playerTurn, actions, boardSize=10) {
         super();
         this.status = 'pre-game';
-        this.state = {
-            boardSize : 100
-        }
-
+        
         this.gameId = hashId();
         // this.boards = []
         // this.players = players;
         // this.boardSize = boardSize;
         // this.playerTurn = playerTurn;
         // this.actions = [];
-
+        
         this.gameInit = function(){
             this.boards = new Array(2).fill(new Board(this.boardSize));
             this.actions = [];
+        }
+        
+        this.state = {
+            boardSize : 100
         }
     }
 
@@ -46,11 +47,9 @@ class Game extends React.Component {
             <div className="main">
                 <div className="button-selectors">
                         <h1>BattleShip</h1>
-                    {/* <div className="button-selectors"> */}
                         <h4>Please select board size</h4>
                         <button value={10} onClick={(e) => this.boardSizeHandler(e.target.value)}>10X10</button>
                         <button value={14} onClick={(e) => this.boardSizeHandler(e.target.value)}>14X14</button>
-                    {/* </div> */}
                 </div>
                 <div className="boards-place">
                     <Board boardSize={this.state.boardSize} subsConfig={this.subsConfig} status={this.status} /> {/*Choosing ships board*/}
