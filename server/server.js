96b8fd5982714112160e03d51a690970df1ff8f7
Liron <liron2009@gmail.com>
2020-10-13 08:49:16 +0300

    url env update to heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const os = require('os');
 const path = require('path');
 
 app.use(express.static(path.join(__dirname,'..' ,'build')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
